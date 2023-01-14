4429796785632829b59a5b943a0ca52ccc3e6da0
besnadne <serpov.box@gmail.com>
2023-01-14 20:57:08 +0200

    create componets

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Card from './components/Card';
+import Header from './components/Header';
+import Drawer from './components/Drawer';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App clear">
+      
+      <Drawer/>
+
+      <Header/>
+      <div className="content">
+        <div className="h1-search">
+          <h1>Все кроссовки</h1>
+          <div className="search-block">
+            <img src="/img/search-block.svg" alt="" />
+            <input placeholder="Поиск..." type="text" />
+          </div>  
+        </div>
+        <div className="sneackers">
+          <Card/>
+          <Card/>
+          <Card/>
+          <Card/>
+        </div>
+        ...
+      </div>
     </div>
   );
 }
